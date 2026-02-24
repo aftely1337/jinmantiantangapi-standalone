b53639a3643842634aebc2dd81462c1a7c73ab57
aftely1337 <2387707025@qq.com>
2026-02-24 23:28:12 +0800

    Fix Gradle configuration for Android build

---
+++ app/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+
+android {
+    namespace = "eu.kanade.tachiyomi.extension.zh.jinmantiantangapi"
+    compileSdk = 34
+    
+    defaultConfig {
+        applicationId = "eu.kanade.tachiyomi.extension.zh.jinmantiantangapi"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+    
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+    
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    // Tachiyomi dependencies
+    compileOnly("org.jetbrains.kotlin:kotlin-stdlib:1.9.22")
+    compileOnly("com.github.tachiyomiorg:extensions-lib:1.5")
+    compileOnly("com.squareup.okhttp3:okhttp:4.12.0")
+    compileOnly("androidx.preference:preference-ktx:1.2.1")
+}
