35961db5dc6e576a46fe123332df1e77c0e6c9d0
aftely1337 <2387707025@qq.com>
2026-02-24 23:32:09 +0800

    Fix multi-module Gradle configuration and workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,49 +1,27 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+    }
+}
+
 plugins {
-    id("com.android.application")
-    kotlin("android")
+    id("com.android.application") version "8.2.2" apply false
+    kotlin("android") version "1.9.22" apply false
 }
 
-android {
-    namespace = "eu.kanade.tachiyomi.extension.zh.jinmantiantangapi"
-    compileSdk = 34
-    
-    defaultConfig {
-        applicationId = "eu.kanade.tachiyomi.extension.zh.jinmantiantangapi"
-        minSdk = 21
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
-    }
-    
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-        }
-    }
-    
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    
-    kotlinOptions {
-        jvmTarget = "17"
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
-dependencies {
-    // Tachiyomi dependencies
-    compileOnly("org.jetbrains.kotlin:kotlin-stdlib:1.9.22")
-    compileOnly("com.github.tachiyomiorg:extensions-lib:1.5")
-    compileOnly("com.squareup.okhttp3:okhttp:4.12.0")
-    compileOnly("androidx.preference:preference-ktx:1.2.1")
-    
-    // JSON parsing
-    implementation("org.json:json:20231013")
-}
-
-repositories {
-    google()
-    mavenCentral()
-    maven { url = uri("https://jitpack.io") }
+tasks.register("clean", Delete::class) {
+    delete(rootProject.layout.buildDirectory)
 }
