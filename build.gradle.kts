0e1af5a6eb36c9ccfe84fa79b1800df60f06ddc6
aftely1337 <2387707025@qq.com>
2026-02-25 01:03:56 +0800

    Update root build.gradle.kts to Kotlin 2.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,8 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:2.3.0")
     }
 }
 
